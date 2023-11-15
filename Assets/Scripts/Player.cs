6baed019c1b928ae917329f3e0fdfd9b2766050a
Thales Raymond <thalesfraymond@gmail.com>
2023-11-15 17:38:55 -0300

    player input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float Speed = 6f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right);
+        //transform.Translate(Vector3.right * this.Speed * Time.deltaTime * Input.GetAxis("Horizontal"));
+
+        //transform.Translate(Vector3.up * this.Speed * Time.deltaTime * Input.GetAxis("Vertical"));
+
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
+
+        var movimentVector = this.Speed * Time.deltaTime * new Vector3(horizontalInput, verticalInput);
+
+        transform.Translate(movimentVector);
     }
 }
