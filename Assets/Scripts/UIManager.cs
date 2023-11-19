848b4196b90cd2322d5cf2cc33dac16ed6a04921
Thales Raymond <thalesfraymond@gmail.com>
2023-11-19 19:42:43 -0300

    game over text

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -17,12 +17,17 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Image _currentLifeCount;
 
+    [SerializeField]
+    private Text _gameOverText;
+
     // Start is called before the first frame update
     void Start()
     {
         this._player = GameObject.Find("Player").GetComponent<Player>();
 
         this.UpdateScoreText(0);
+
+        this._gameOverText.gameObject.SetActive(!this._player.IsAlive());
     }
 
     // Update is called once per frame
@@ -30,6 +35,8 @@ public class UIManager : MonoBehaviour
     {
         this.UpdateScoreText(this._player.Score);
         this.UpdateDisplayedLives();
+
+        this._gameOverText.gameObject.SetActive(!this._player.IsAlive());
     }
 
     private void UpdateScoreText(int score)
